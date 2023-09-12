6db64b02928eda060e83993d769a566bdef64764
n.samotskiy <n.samotskiy@rambler-co.ru>
2023-09-12 17:13:34 +0300

    first test template added

---
+++ src/test/java/org/example/TestTemplate.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+public class TestTemplate {
+
+    @Test
+    public void testTemplate() {
+        System.out.println("Hello from a first test!");
+    }
+}
